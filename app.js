2b55cfbab75643435377a068ca745fd881bfbbf8
sulaiman <sulaiman@pop-os.localdomain>
2023-09-27 23:20:47 +0530

    update

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 
 
+
 const posted=document.querySelector('.post2')
 
 const inputs=document.querySelector('#postwriting')
@@ -16,21 +17,94 @@ btn.addEventListener('click',()=>{
     inputs.value=""
 })
 
-//
-
-const cards = document.querySelectorAll('.cards')
-const card = document.querySelectorAll('.card')
-const mycard =document.getElementById('mycard')
-
-const cir = document.querySelectorAll('.cir')
 
 
-const input = document.getElementById('btn')
 
-console.log(card)
-input.addEventListener('input',(e)=>{ 
-    let index = e.target
-    console.log(index)
-    mycard.style.backgroundImage = `url(${URL.createObjectURL(input.files[0])})` 
 
-})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const cards = document.querySelector('.cards')
+const upload = document.getElementById('upload')
+const rightCard = document.getElementsByClassName('right-cards')[0]
+
+let index = 0
+function createCard(){
+
+    // creating Element
+    const cardDiv = document.createElement('div')
+    const input = document.createElement('input')
+    const label = document.createElement('label')
+    const btndiv = document.createElement('div')
+    const cir = document.createElement('div')
+
+
+    // attribute
+    input.type = 'file'
+    input.id = `btn-${index}`
+    input.className = 'btn'
+    input.accept = 'image/*'
+    label.htmlFor = `btn-${index}`
+    label.innerHTML = 'upload'
+
+
+    // class & id 
+    btndiv.className = 'btndiv'
+    cardDiv.className = `card-${index}`
+    cardDiv.className = `card`
+    cir.className = 'cir'
+
+    console.log(cardDiv)
+
+    // append child
+    btndiv.appendChild(label)
+    btndiv.appendChild(input)
+    cardDiv.appendChild(cir)
+    cardDiv.appendChild(btndiv)
+    rightCard.appendChild(cardDiv)
+
+    // eveneListener
+    input.addEventListener('input',()=>{
+
+        const card = input.parentElement.parentElement;
+        console.log(input)
+     cardDiv.style.backgroundImage = `url(${URL.createObjectURL(input.files[0])})` 
+        index++;
+    })
+   
+}
+
+upload.addEventListener('click',createCard)
+
+
+
